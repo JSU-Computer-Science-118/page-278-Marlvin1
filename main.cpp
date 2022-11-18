932c344355d3ab96f1accf3b53b10865a4be7e17
Marlvin1 <111468915+Marlvin1@users.noreply.github.com>
2022-11-18 05:30:08 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -13,42 +13,31 @@ const string SENTINEL = "-1";
 int main()
 {
     string name;
-    int numOfVolunteers;
-    int numOfBoxesSold;
-    int totalNumOfBoxesSold;
+    int numOfVolunteers = 0;
+    int numOfBoxesSold = 0;
+    int totalNumOfBoxesSold = 0;
     double costOfOneBox;
-    
+ 
     cout<<fixed<<showpoint<<setprecision(2);
-    
-    cout<<"Line 14: Enter each volunteers name and the number of boxes"<<endl<<"    sold by each volunteer, ending with -1: "<<endl;
-    
-    totalNumOfBoxesSold =0;
-    numOfVolunteers = 0;
-    
-    cin>>name;
-    
-    while(name != SENTINEL)
+    string * ptr = nullptr;
+    ptr = new string;
+    cin>> *ptr;
+    while (*ptr != SENTINEL)
     {
-        cin>>numOfBoxesSold;
-        totalNumOfBoxesSold = totalNumOfBoxesSold + numOfBoxesSold;
         numOfVolunteers++;
-        cin>>name;
+        cin>>numOfBoxesSold;
+        totalNumOfBoxesSold += numOfBoxesSold;
+        cin>> *ptr;
     }
-    cout<<endl;
-    
-    cout<<"Line 26: The total number of boxes sold: "<<totalNumOfBoxesSold<<endl;
-    
-    cout<<"Line 27: Enter the cost of one box: ";
     cin>>costOfOneBox;
-    cout<<endl;
-    
-    cout<<"Line 30: The total money made by selling cookies: $"<<totalNumOfBoxesSold*costOfOneBox<<endl;
-    
-    if (numOfVolunteers != 0 )
-    {
-        cout<<"Line 32: The average number of boxes sold by each volunteer: "<<totalNumOfBoxesSold/numOfVolunteers<<endl;
-    }
+    cout<<"Total number of boxes sold: "<<totalNumOfBoxesSold<<endl;
+    cout<<"Total money made: $"<<totalNumOfBoxesSold*costOfOneBox<<endl;
+    if (numOfVolunteers != 0)
+        cout<<"Average number of boxes sold by each person: "<<totalNumOfBoxesSold/numOfVolunteers<<endl;
+    else
+        cout<<"No input."<<endl;
+ 
+
 
     return 0;
 }
-
